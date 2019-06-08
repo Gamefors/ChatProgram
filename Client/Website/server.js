c23a088652d1848eefe0d1dd5d2d5f4965dcd01e
Jan G.J <gameforsz@gmail.com>
2019-06-08 21:00:46 +0200

    change ip to y

--- Client/Website/server.js
+++ Client/Website/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "192.168.0.117",
   user: "chatprogram",
   password: "safepw",
   database: "chat"
