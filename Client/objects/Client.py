484d66751c1e044d47457f4825e24cb3cc0f4c3e
Jan G.J <gameforsz@gmail.com>
2019-06-08 21:02:06 +0200

    removed client

--- Client/objects/Client.py
+++
@@ -1,7 +0,0 @@
-class ClientObject:
-    def __init__(self, username, socketObject, ip, port, channel):
-        self.username = username
-        self.socketObject = socketObject
-        self.ip = ip
-        self.port = port
-        self.channel = channel
