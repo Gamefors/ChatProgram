b0035ce5eb2dc763b75128fecc3812758cb791a7
Jan G.J <gameforsz@gmail.com>
2019-08-17 22:07:27 +0200

    added automatic creation of mysql table

--- Server/utils/MysqlHelper.py
+++ Server/utils/MysqlHelper.py
@@ -1,5 +1,5 @@
 from utils.FileHelper import FileHelper#pylint: disable=E0611,E0401
-
+from utils.LogHelper import LogHelper#pylint: disable=E0611,E0401
 import mysql.connector
 
 class MysqlStatement:
@@ -10,7 +10,14 @@ class MysqlStatement:
 		self.cur = self.cnx.cursor()
 
 	def execute(self):
-		self.cur.execute(self.stmt)
+		try:
+			self.cur.execute(self.stmt)
+		except mysql.connector.errors.ProgrammingError as ex:
+			if "Table 'chat.accounts' doesn't exist" in str(ex):
+				return "createTable"
+			else:
+				print(ex)
+				
 		return self
 
 	def escape(self):
@@ -34,6 +41,7 @@ class MysqlHelper:
 
 	def __init__(self):
 		self.fileHelper = FileHelper()
+		self.logHelper = LogHelper()
 		config = self.fileHelper.getConfig("Mysql Server Config")
 		try:
 			self.connection = mysql.connector.connect(user = config.username , password = config.password, host = config.ip, database = config.database)
@@ -41,7 +49,13 @@ class MysqlHelper:
 			print("Couldn't establish connection to mysql database(" + config.database + ") with ip: " + config.ip)#TODO: find a way to handle with this
 	
 	def ExecuteCommand(self,command):
-		result = MysqlStatement(command ,self.connection).execute().escape().fetchall().result
+		result = None
+		try:
+			result = MysqlStatement(command ,self.connection).execute().escape().fetchall().result
+		except AttributeError:
+			self.logHelper.log("info", "Created mysql table.")
+			self.ExecuteCommandWithoutFetchAndResult("CREATE TABLE accounts ( id INT NOT NULL AUTO_INCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, rank TEXT NOT NULL, loggedIn TINYINT NOT NULL DEFAULT '0', PRIMARY KEY (id))")
+
 		return result
 
 	def ExecuteCommandWithoutFetchAndResult(self, command):
@@ -49,7 +63,12 @@ class MysqlHelper:
 
 	def tryLogin(self, clientObject, password):#TODO: give better fedback for layer 8 
 		result = False
-		if len(self.ExecuteCommand("SELECT * FROM accounts WHERE username = '" + clientObject.username + "'")) > 0:
+		lenght = None
+		try:
+			lenght = len(self.ExecuteCommand("SELECT * FROM accounts WHERE username = '" + clientObject.username + "'"))
+		except:
+			lenght = len(self.ExecuteCommand("SELECT * FROM accounts WHERE username = '" + clientObject.username + "'"))
+		if lenght > 0:
 			if self.ExecuteCommand("select loggedIn,(case when loggedIn = 0 then 'loggedOut' when loggedIn = 1 then 'loggedIn' end) as loggedIn_status FROM accounts WHERE username = '" + clientObject.username + "'")[0][1] != "loggedIn":
 				if len(self.ExecuteCommand("SELECT * FROM accounts WHERE username = '" + clientObject.username + "' and password = '" + password + "'")) > 0:
 					self.ExecuteCommandWithoutFetchAndResult("UPDATE accounts SET loggedIn = 1 WHERE username = '" + clientObject.username + "'")
